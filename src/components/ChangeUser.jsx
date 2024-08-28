a85d41ad995e82ed827f21195fb2c444905579fe
Armine Muradyan <muradyan.armin13@gmail.com>
2024-08-28 16:55:22 +0400

    add new functional input for changing name

--- src/components/ChangeUser.jsx
+++ src/components/ChangeUser.jsx
@@ -1,12 +1,26 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 
 function ChangeUser() {
-  const { user, setUser } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleClick = () => {
+    if (inputValue.trim() !== "") {
+      setUser(inputValue);
+      setInputValue("");
+    }
+  };
   return (
-    <button onClick={() => setUser(user === "Armine" ? "Avik" : "Armine")}>
-      Change User
-    </button>
+    <>
+      <input
+        value={inputValue}
+        type="text"
+        placeholder="Add a Name"
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+      <button onClick={handleClick}>Change User</button>
+    </>
   );
 }
 
